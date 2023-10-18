1b9b1d55b885baee42595103ef1408bcf12a3a0d
lausan3 <lauanthony4455@gmail.com>
2023-10-18 00:54:57 -0400

    Trying to draw a ray that looks in the direction the player is looking

--- Assets/Scripts/Grappler.cs
+++ Assets/Scripts/Grappler.cs
@@ -3,15 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// THIS SCRIPT ASSUMES YOU HAVE A MOVEMENT SCRIPT ATTACHED TO THE PLAYER
 public class Grappler : MonoBehaviour
 {
     [Header("Grappling adjustments")] 
     [Tooltip("How far the grappler can shoot")] public float grappleRange = 5f;
     public KeyCode grappleKey;
     public LayerMask grappleLayer;
-    public float jointSpringForce;
-    public float jointDamperForce;
-    public float jointMassScale;
+    public float jointSpringForce = 5f;
+    public float jointDamperForce = 0.5f;
+    public float jointMassScale = 1f;
 
     
     [Header("References")]
@@ -19,12 +21,17 @@ public class Grappler : MonoBehaviour
     [Tooltip("Player Transform")] public Transform player;
     public Transform playerCamera;
     [Tooltip("Line Renderer component to cast a visible line")] public LineRenderer lineRenderer;
+    private Movement mvment;
 
     private SpringJoint joint;
     private RaycastHit hit;  // stores hit info
     private Vector3 grapplePoint;
     [HideInInspector] public bool grappling;  // public flag that denotes if we are currently trying to grapple
 
+    private void Start()
+    {
+        mvment = player.GetComponent<Movement>();
+    }
 
     private void Update()
     {
@@ -55,8 +62,9 @@ public class Grappler : MonoBehaviour
     private void StartGrapple()
     {
         // check raycast to see if grappleable object is in grapple range
-        if (Physics.Raycast(player.position, playerCamera.forward, out hit, grappleRange, grappleLayer))
+        if (Physics.Raycast(player.position, mvment.lookDir, out hit, grappleRange, grappleLayer))
         {
+            Debug.Log("Grappled!");
             grapplePoint = hit.point;
             
             // Grapple joint
