aab6ac4b69d7b75e16cdd112c2f16faa94f4c1ec
ChillBeanJosh <joshover23@gmail.com>
2023-10-18 22:52:34 -0400

    Update Dash.cs

--- Assets/Scripts/Dash.cs
+++ Assets/Scripts/Dash.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class Dash : MonoBehaviour
 {
-    public CharacterController player;
     public float distance;
-
+    Rigidbody rig;
     private bool dashing = true;
+
     public float dashCooldown;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -29,11 +29,8 @@ public class Dash : MonoBehaviour
     }
 
     private IEnumerator dash()
-    { 
-        dashing = true;
-
-        Vector3 move = transform.forward;
-        player.Move(move * distance);
+    {
+        rig.AddForce(transform.forward * distance, ForceMode.Impulse);
         dashing = false;
 
         yield return new WaitForSeconds(dashCooldown);
